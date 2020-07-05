b5ba19c877d91716946423849e019d3da42f5c52
whitehorse1234 <58981238+whitehorse1234@users.noreply.github.com>
2020-07-05 17:23:09 +0800

    add function

--- src/views/excel/export-excel.vue
+++ src/views/excel/export-excel.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="app-container">
-
     <div>
       <FilenameOption v-model="filename" />
       <AutoWidthOption v-model="autoWidth" />
       <BookTypeOption v-model="bookType" />
-      <el-button :loading="downloadLoading" style="margin:0 0 20px 20px;" type="primary" icon="el-icon-document" @click="handleDownload">
-        Export Excel
-      </el-button>
-      <a href="https://panjiachen.github.io/vue-element-admin-site/feature/component/excel.html" target="_blank" style="margin-left:15px;">
+      <el-button
+        :loading="downloadLoading"
+        style="margin:0 0 20px 20px;"
+        type="primary"
+        icon="el-icon-document"
+        @click="handleDownload"
+      >Export Excel</el-button>
+      <a
+        href="https://panjiachen.github.io/vue-element-admin-site/feature/component/excel.html"
+        target="_blank"
+        style="margin-left:15px;"
+      >
         <el-tag type="info">Documentation</el-tag>
       </a>
     </div>
 
-    <el-table v-loading="listLoading" :data="list" element-loading-text="Loading..." border fit highlight-current-row>
-      <el-table-column align="center" label="Id" width="95">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading..."
+      border
+      fit
+      highlight-current-row
+    >
+      <!-- <el-table-column align="center" label="Id" width="95">
         <template slot-scope="scope">
           {{ scope.$index }}
         </template>
-      </el-table-column>
+      </el-table-column>-->
       <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.title }}</template>
       </el-table-column>
       <el-table-column label="Author" width="110" align="center">
         <template slot-scope="scope">
@@ -30,9 +42,7 @@
         </template>
       </el-table-column>
       <el-table-column label="Readings" width="115" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.pageviews }}</template>
       </el-table-column>
       <el-table-column align="center" label="Date" width="220">
         <template slot-scope="scope">
@@ -45,65 +55,73 @@
 </template>
 
 <script>
-import { fetchList } from '@/api/article'
-import { parseTime } from '@/utils'
+import { fetchList } from "@/api/article";
+import { parseTime } from "@/utils";
 // options components
-import FilenameOption from './components/FilenameOption'
-import AutoWidthOption from './components/AutoWidthOption'
-import BookTypeOption from './components/BookTypeOption'
+import FilenameOption from "./components/FilenameOption";
+import AutoWidthOption from "./components/AutoWidthOption";
+import BookTypeOption from "./components/BookTypeOption";
 
 export default {
-  name: 'ExportExcel',
+  name: "ExportExcel",
   components: { FilenameOption, AutoWidthOption, BookTypeOption },
   data() {
     return {
       list: null,
       listLoading: true,
       downloadLoading: false,
-      filename: '',
+      filename: "",
       autoWidth: true,
-      bookType: 'xlsx'
-    }
+      bookType: "xlsx",
+    };
   },
   created() {
-    this.fetchData()
+    this.fetchData();
   },
   methods: {
     fetchData() {
-      this.listLoading = true
-      fetchList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
+      this.listLoading = true;
+      fetchList().then((response) => {
+        this.list = response.data.items;
+        this.listLoading = false;
+      });
     },
     handleDownload() {
-      this.downloadLoading = true
-      import('@/vendor/Export2Excel').then(excel => {
-        const tHeader = ['Id', 'Title', 'Author', 'Readings', 'Date']
-        const filterVal = ['id', 'title', 'author', 'pageviews', 'display_time']
-        const list = this.list
-        const data = this.formatJson(filterVal, list)
+      this.downloadLoading = true;
+      import("@/vendor/Export2Excel").then((excel) => {
+        const tHeader = ["Id", "Title", "Author", "Readings", "Date"];
+        const filterVal = [
+          "id",
+          "title",
+          "author",
+          "pageviews",
+          "display_time",
+        ];
+        const list = this.list;
+        const data = this.formatJson(filterVal, list);
         excel.export_json_to_excel({
           header: tHeader,
           data,
           filename: this.filename,
           autoWidth: this.autoWidth,
-          bookType: this.bookType
-        })
-        this.downloadLoading = false
-      })
+          bookType: this.bookType,
+        });
+        this.downloadLoading = false;
+      });
     },
     formatJson(filterVal, jsonData) {
-      return jsonData.map(v => filterVal.map(j => {
-        if (j === 'timestamp') {
-          return parseTime(v[j])
-        } else {
-          return v[j]
-        }
-      }))
-    }
-  }
-}
+      return jsonData.map((v) =>
+        filterVal.map((j) => {
+          if (j === "timestamp") {
+            return parseTime(v[j]);
+          } else {
+            return v[j];
+          }
+        })
+      );
+    },
+  },
+};
 </script>
 
 <style>
